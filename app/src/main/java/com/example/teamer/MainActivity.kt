3731065c6e21d8fea276377fe4895c35db79d28b
William Edmisten <wcedmisten@gmail.com>
2019-05-08 17:29:19 -0400

    Added some navigation improvements

--- app/src/main/java/com/example/teamer/MainActivity.kt
+++ app/src/main/java/com/example/teamer/MainActivity.kt
@@ -3,12 +3,12 @@ package com.example.teamer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
 import com.example.teamer.data.FriendRequest
 import com.example.teamer.model.TeamerVM
 import com.example.teamer.service.FriendRequestMessagingService
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
--- app/src/main/java/com/example/teamer/view/WelcomeFragment.kt
+++ app/src/main/java/com/example/teamer/view/WelcomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.teamer.view
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -39,6 +40,7 @@ class WelcomeFragment : Fragment() {
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         vm.getCurrentUserData().observe(this@WelcomeFragment, Observer {
+                            Log.d("hi2", Navigation.findNavController(viewF).currentDestination?.label.toString())
                             Navigation.findNavController(viewF).navigate(
                                 R.id.action_welcomeFragment_to_friendListFragment
                             )
